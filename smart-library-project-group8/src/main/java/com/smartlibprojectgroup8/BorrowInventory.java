ae443024a25a01f60962523dff8430dfaac97e89
Muhd. Nuqman <abdhakimfnsr@gmail.com>
2026-06-12 23:03:55 +0800

    comment

--- smart-library-project-group8/src/main/java/com/smartlibprojectgroup8/BorrowInventory.java
+++ smart-library-project-group8/src/main/java/com/smartlibprojectgroup8/BorrowInventory.java
@@ -3,12 +3,14 @@ import java.util.ArrayList;
 
 public class BorrowInventory extends ArrayList<Book> {
     @Override
+    // Add book into the Borrowing Inventory
     public boolean add(Book newBook){
         if (search(newBook.getIsbn()) != null) return true;
         Book clonedBook = new Book(newBook.getIsbn(), newBook.getTitle(), newBook.getAuthor());
         return super.add(clonedBook);
     }
 
+    // Search book by ISBN
     public Book search(int isbn) {
         for (Book currentBook: this) {
             if (currentBook.getIsbn() == isbn){
@@ -18,6 +20,7 @@ public class BorrowInventory extends ArrayList<Book> {
         return null;
     }
 
+    // Remove book
     public Book removeBook(int isbn) {
         Book foundBook = search(isbn);
         if (foundBook != null) {
@@ -27,6 +30,7 @@ public class BorrowInventory extends ArrayList<Book> {
         return null;
     }
 
+    // Print the list of books inside the Borrwing Inventory
     public void printList() {
         if (isEmpty()) {
             System.out.println("You have no borrowed books.");
