85590c221b6f0beefe2878c744136e6fffb3f54e
Raja-Athira <23096415@siswa.um.edu.my>
2026-06-12 14:36:17 +0800

    Update Book.java

--- smart-library-project-group8/src/main/java/com/smartlibprojectgroup8/Book.java
+++ smart-library-project-group8/src/main/java/com/smartlibprojectgroup8/Book.java
@@ -24,32 +24,53 @@ public class Book {
         this.author = author; 
     // parent, left, right are null by default
     }
-
+    // return book's ISBN title
     public int getIsbn() { return isbn; }
+    // return book's title
     public String getTitle() { return title; }
+    // return book's author
     public String getAuthor() { return author; }
+    // return left child node (smaller ISBN)
     public Book getLeft() { return left; }
+    // return right child node (larger ISBN)
     public Book getRight() { return right; }
+    // return parent node
     public Book getParent() { return parent; }
 
+    /* Sets the left child and establishes parent-child relationship, 
+     left Book node to become left child */
+    
     public void setLeft(Book left) {
         this.left = left;
         if (left != null) {
+         // Maintain bidirectional link
             left.setParent(this);
         }
     }
+    parent; }
+
+    /* Sets the right child and establishes parent-child relationship, 
+     right Book node to become right child */
 
     public void setRight(Book right) {
         this.right = right;
         if (right != null) {
+         // Maintain bidirectional link
             right.setParent(this);
         }
     }
 
+   /*Sets the parent node (used when inserting/deleting), 
+     Book node to become parent
+     */
     public void setParent(Book parent) {
         this.parent = parent;
     }
-
+ 
+/* Removes a child node (left or right) by setting to null, 
+     Used during deletion operations,
+      The child node to remove (must be left or right)
+     */
     public void removeChild(Book child) {
         if (left.equals(child)) left = null;
         if (right.equals(child)) right = null;
